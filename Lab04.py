d30dcb4819e725d7f3c12c92b9d2d98350e6bbae
mayuri <ubale.mayuri@msds.christuniversity.in>
2023-09-05 18:05:53 +0530

    update

--- Lab04.py
+++ Lab04.py
@@ -1,9 +1,26 @@
-def Gsms(name,product,quantity):
-    file=open("Gsms.txt","a+")
-    file.write("Name\t:\t"+name)
-    file.write("\nproduct\t:\t"+product)
-    file.write("\nquantity\t:\t"+quantity)
-name=input("enter your name")
-product=input("enter product you want from gsms")
-quantity=input("enter quantity")
-Gsms(name,product,quantity)
+
+def gsms(product,price,quantity,total,custname,phone):
+    filename = open("LAB04.txt","a+")
+    filename.write("Product Name\t: \t"+product)
+    filename.write("\nPrice\t: "+price)
+    filename.write("\nQuantity\t: \t"+quantity)
+    filename.write("\nTotal Amount\t: \t"+total)
+    filename.write("\nName of the customer\t: \t"+custname)
+    filename.write("\nPhone number\t: \t"+phone)
+    filename.close()
+    return True
+item = input("Enter the product: ")
+price = input("Item Price: ")
+qty = input("Number of Products: ")
+total = input("Total amount is: ")
+cname = input("Enter the name of the customer: ")
+mob = input("Enter the phone number of the customer: ")
+gms =gsms(product,price,quantity,total,custname,phone)
+if gms:
+    print("Thank you for Grocery Shopping")
+    print("---------------------------------------------")
+    for line in open("LAB04.txt", "r+"). readlines() :
+        print(line)
+        print("---------------------------------------------")
+else:
+    print("error")
